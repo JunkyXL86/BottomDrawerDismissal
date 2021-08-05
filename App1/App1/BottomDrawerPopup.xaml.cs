1418382db2d089f682f57f482eb3899748e64360
Daniel Frahn <daniel.frahn@appcore.de>
2021-08-05 11:51:44 +0200

    Add project files.

---
+++ App1/App1/BottomDrawerPopup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BottomDrawerPopup
+    {
+        private const double MinimumTranslationY = 10d;
+
+        public BottomDrawerPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    Frame.TranslationY = Math.Max(MinimumTranslationY, Frame.TranslationY + e.TotalY);
+                    break;
+            }
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBottomDrawerButton_Clicked(object sender, EventArgs e)
+        {
+            this.ShowPopup(new BottomDrawerPopup());
+        }
+    }
+}
